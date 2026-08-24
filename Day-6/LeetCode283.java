c7148d43d6356448b387c443fe1c7f80e6f2b942
Vaachi Goel <159532195+Vaachi405@users.noreply.github.com>
2026-08-24 10:43:56 +0000

    till day 7 code added

---
+++ Day-6/LeetCode283.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int len=nums.length;
+        int i=0;
+        int k=0;
+        for(i=0;i<len;i++)
+        {
+            if(nums[i]!=0)
+            {
+                int temp=nums[k];
+                nums[k]=nums[i];
+                nums[i]=temp;
+                k++;
+            }
+        }
+    }
+}
